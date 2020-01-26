5b2391de3fd478d54ad540c88a0a8d9125080620
Jongho Jeon <jongho.jeon@riiid.co>
2020-01-27 03:34:24 +0900

    Added exposed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
 allprojects {
     repositories {
         mavenCentral()
+        jcenter()
     }
 }
 
--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,1 +1,3 @@
-
+dependencies {
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.20.3")
+}
